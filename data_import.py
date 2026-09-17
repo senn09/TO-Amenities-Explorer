90b040eed10a9a986b4f5cd84b580c264c0aadfa
Steve Nhan <nhann.steve@gmail.com>
2026-09-17 13:15:35 -0400

    data_import bug fix

--- data_import.py
+++ data_import.py
@@ -3,13 +3,15 @@ import pandas as pd
 from io import StringIO
 
 base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+
+col_of_interest = ['name', 'address', 'type_id']
         
 params =    [{ 
                 "id": "library-branch-general-information",
                 "type_id": 1,
                 "col_name_transform": [
-                    ('BranchName', 'name'),
-                    ('Address', 'address'),
+                    ('BranchName', col_of_interest[0]),
+                    ('Address', col_of_interest[1]),
                 ],
                 "col_of_interest": [
                     'BranchName', 
@@ -24,9 +26,9 @@ params =    [{
                 "id": "parks-and-recreation-facilities",
                 "type_id": 2,
                 "col_name_transform": [
-                    ('ASSET_NAME', 'name'),
-                    ('ADDRESS', 'address'),
-                    ('TYPE', 'type_id'),
+                    ('ASSET_NAME', col_of_interest[0]),
+                    ('ADDRESS', col_of_interest[1]),
+                    ('TYPE', col_of_interest[2]),
                 ],
                 "col_of_interest": [
                     'ASSET_NAME',
@@ -59,14 +61,11 @@ def format_data_for_db(param, df):
     for i in range(len(param['col_name_transform'])):
         df.rename(columns={param['col_name_transform'][i][0]: param['col_name_transform'][i][1]}, inplace=True)
 
-    # there is only two columns atm but I think doing this systematically would be good practice down the line
-    col_of_interest = [pair[1] for pair in param['col_name_transform']]
     if param['type_id'] == 2:
-        df['type_id'].replace({'Park': 2, 'Community Centre': 3, 'Civic Centre': 4})
+        df['type_id'] = df['type_id'].replace({'Park': 2, 'Community Centre': 3, 'Civic Centre': 4})
     else:
         df['type_id'] = param['type_id']
 
-    print(f'formatted {param['id']}')
     return df[col_of_interest]   
 
 
